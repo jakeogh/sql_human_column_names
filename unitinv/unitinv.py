7f48b5c29b2e2c2206fa046ef7cb87c95ee4b8f5
jakeogh <github.com@v6y.net>
2021-02-12 23:26:42 -0700

    auto-commit

--- unitinv/unitinv.py
+++ unitinv/unitinv.py
@@ -134,7 +134,7 @@ def list_postgresql_column_types(ctx, all_types):
                 conn.execute(text('select * from pg_type'))
         else:
             results = \
-                conn.execute(text('select pg_type.typname, pg_type.typlen, pg_type.typtype from pg_type where typisdefined and typarray!=:z and typtype!=:a'), {"z": 0, "a": 'd'})
+                conn.execute(text('select pg_type.typname, pg_type.typlen, pg_type.typtype from pg_type where typisdefined and not typarray and typtype!=:a'), {"z": 0, "a": 'd'})
                 #conn.execute(text('select pg_type.typname, pg_type.typlen, pg_type.typtype from pg_type where typisdefined=:y and typarray!=:z and typtype!=:a'), {"y": True, "z": 0, "a": 'd'})
         for result in results:
             print(result)
